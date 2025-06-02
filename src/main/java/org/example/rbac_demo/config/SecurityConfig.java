1479b5506f777a3d7f6b66322090bc0518f57756
Darius Bahrami <Darius.Bahrami@Student.reutlingen-university.DE>
2025-06-02 16:10:38 +0200

    DEMO

--- src/main/java/org/example/rbac_demo/config/SecurityConfig.java
+++ src/main/java/org/example/rbac_demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.rbac_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,18 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
-                        .requestMatchers("/login", "/css/**","/js/**")
-                        .permitAll()
                         .anyRequest().authenticated()
                 )
-
-                .formLogin(form -> form
-                        .loginPage("/login") // Optional: customize login page
-                        .permitAll()
-                )
+                .formLogin(Customizer.withDefaults()) // <-- default login page
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .permitAll()
